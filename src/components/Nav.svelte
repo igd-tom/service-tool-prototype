85b63e10f2ca2d1e7487029c2da194be506968c7
Tom <fincht96@gmail.com>
2020-12-09 10:43:05 +0000

    Created sapper project

---
+++ src/components/Nav.svelte
@@ -0,0 +1,64 @@
+<script>
+	export let segment;
+
+	const handleClick = () => {
+		console.log(segment);
+	};
+</script>
+
+<style>
+	nav {
+		background: #fff3ef;
+	}
+	nav div {
+		max-width: 960px;
+		padding: 0 10px;
+		margin: 0 auto;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		align-items: center;
+	}
+	h1 {
+		margin-top: 10px;
+		font-size: 22px;
+		font-weight: bold;
+	}
+	ul {
+		padding: 0;
+		margin: 0;
+		text-align: right;
+	}
+	li {
+		display: inline-block;
+	}
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+	a.current {
+		border-bottom: 3px solid #ff3e00;
+	}
+</style>
+
+<nav>
+	<div>
+		<h1 on:click={handleClick}>Service Tool</h1>
+
+		<ul>
+			<li><a class:current={segment === undefined} href=".">home</a></li>
+			<li>
+				<a class:current={segment === 'about'} href="about">about</a>
+			</li>
+
+			<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+				 the blog data when we hover over the link or tap it on a touchscreen -->
+			<li>
+				<a
+					rel="prefetch"
+					class:current={segment === 'blog'}
+					href="blog">blog</a>
+			</li>
+		</ul>
+	</div>
+</nav>
